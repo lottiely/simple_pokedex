480440acc7beeda1d8bdf5f746454ca5de0fdb43
lottiely <saialmario1@gmail.com>
2019-10-18 14:30:41 -0700

    Updated requirements.txt.

--- Pokedex/settings.py
+++ Pokedex/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = cfg.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://simplepokedexapp.herokuapp.com/']
 
 
 # Application definition
